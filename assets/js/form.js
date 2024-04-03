572c627b19b613b12f70e75ed94965cf87afe564
mariedevoll <marie.devoll@gmail.com>
2024-04-02 20:18:24 -0400

    finished one part of the blog, going to continue and finish the last part of the blog

---
+++ assets/js/form.js
@@ -0,0 +1,43 @@
+const formElement = document.querySelector('form');
+
+const redirectPage = function() {
+    location.href = "./blog.html"
+
+}
+
+const storeBlogDataLocalStorage = function(data) {
+    const blogs = readBlogDataLocalStorage();
+
+    blogs.push(data);
+    const stringData = JSON.stringify(blogs);
+
+    localStorage.setItem("blogs", stringData);
+}
+
+const handleFormSubmit = function(event) {
+    event.preventDefault();
+    const username = document.querySelector('#username').value.trim();
+    const title = document.querySelector('#title').value.trim();
+    const content = document.querySelector('#content').value.trim();
+
+    if (!username || !title || !content) {
+        const errorElement = focument.querySelector('#error');
+        errorElement.textContent = 'please fill in all fields';
+
+        setTimeout(() => {
+            errorElement.textContent = '';
+        }, 3000);
+        return;
+    }
+const formData = {
+    username: username,
+    title: title,
+    content: content
+}
+console.log(formData);
+
+storeBlogDataLocalStorage(formData);
+redirectPage();
+}
+
+formElement.addEventListener('submit', handleFormSubmit)
